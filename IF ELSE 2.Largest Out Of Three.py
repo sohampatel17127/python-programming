f6774bc2a687d69d20f0a0231cf6698937aef4ce
sohampatel17127 <sohampatel17127@gmail.com>
2026-01-17 20:42:49 +0530

    Add files via upload

---
+++ IF ELSE 2.Largest Out Of Three.py
@@ -0,0 +1,22 @@
+a=90
+b=80
+c=45
+if b<a>c:
+    largest = a
+elif a<b>c:
+    largest = b
+else:
+    largest = c
+                          #hamare dono logic sahi hai upar or niche ka 
+
+if a<b and a<c:
+    smallest = a
+elif b<a and b<c:
+    smallest = b
+else:
+    smallest = c
+
+
+
+print("Largest is:",largest)
+print("Smallest is:",smallest)
