fbf78dc52ded63d021c055bfac198662d770f6a4
sohampatel17127 <sohampatel17127@gmail.com>
2026-01-18 20:34:20 +0530

    Add files via upload

---
+++ IF ELSE 1.Largest Out Of Two.py
@@ -0,0 +1,17 @@
+
+a = 56
+b = 94
+
+
+if a>b:
+    print(f"{a} is largest")
+else:
+    print(f"{b} is largest")
+
+if a<b:
+    print(f"{a} is smallest")
+else:
+    print(f"{b} is smallest")
+
+
+
